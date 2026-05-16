f00262b4cea855287940810a256688a6933b6595
Senthilmurugan Paranjothi <senthilmurugan.paranjothi@gmail.com>
2026-05-15 22:06:12 -0400

    First commit

--- src/main/java/com/app/employee/service/UserGrpcService.java
+++ src/main/java/com/app/employee/service/UserGrpcService.java
@@ -2,6 +2,7 @@ package com.app.employee.service;
 
 import com.app.employee.UserRequest;
 import com.app.employee.UserResponse;
+import com.app.employee.AllUserResponse;
 import com.app.employee.UserServiceGrpc;
 import com.app.employee.model.User;
 //import com.app.employee.service.UserDomainService;
@@ -39,6 +40,45 @@ public class UserGrpcService extends UserServiceGrpc.UserServiceImplBase {
 				.build();
 
 		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+	}
+
+	@Override
+	public void getAllUsers(UserRequest request,
+						io.grpc.stub.StreamObserver<AllUserResponse> responseObserver) {
+
+		// Fetch domain object
+		User user = userDomainService.getUserById(request.getUserId());
+		//User user = getUserById(request.getUserId());
+
+		// Map domain → proto
+
+		for (int i = 1; i <= 20; i++) {
+
+			AllUserResponse response = AllUserResponse.newBuilder()
+					.setUserId(String.valueOf(i))   // iteration count as userId
+					.setFirstName(user.getFirstName())
+					.setLastName(user.getLastName())
+					.setEmail(user.getEmail())
+					.setPhone(user.getPhone())
+					.setAddress(user.getAddress())
+					.setCity(user.getCity())
+					.setState(user.getState())
+					.setCountry(user.getCountry())
+					.setZipCode(user.getZipCode())
+					.setRole(user.getRole())
+					.setStatus(user.getStatus())
+					.build();
+
+			responseObserver.onNext(response);
+
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
 		responseObserver.onCompleted();
 	}
 
